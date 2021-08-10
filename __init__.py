23b19b0a05d412d84bf0ea8fbd071fc66bd6bae3
Christoph Flügel <christoph@fluegel.it>
2021-08-10 18:52:26 +0200

    Added an __init__.py in order to make it a package. Is this enough?

---
+++ __init__.py
@@ -0,0 +1,7 @@
+# This does not necessarily need to be a package, but honestly, it hopefully makes it easier to import 
+# this class / module into other projects later down the road. 
+
+# Source: 
+# - https://python-packaging-tutorial.readthedocs.io/en/latest/setup_py.html
+
+from .pFTPDManager import pFTPDManager
